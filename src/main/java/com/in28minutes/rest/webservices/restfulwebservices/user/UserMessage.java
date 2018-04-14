bdc8e753f5f4c2581069ab66b549979bee7136e0
prachikhanna <er.prachikhanna@gmail.com>
2018-04-14 15:41:35 -0500

    spring restful webservice

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserMessage.java
@@ -0,0 +1,16 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+public class UserMessage {
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
